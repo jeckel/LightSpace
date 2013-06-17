ff74cbd07c2cfd39951e00cc33656fb8f5cdf68d
jeckel <jeckel@jeckel.fr>
2013-06-17 21:10:03 +0200

    Configuration for 7 strip of 90 leds with Arduino Mega

--- LightSpace.ino
+++ LightSpace.ino
@@ -7,14 +7,16 @@
 #include "FireEffect.h"
 
 // Nb led
-#define NUM_LEDS 450
-#define NB_STRIPS 5
+//#define NUM_LEDS 450
+//#define NB_STRIPS 5
+#define NUM_LEDS 630
+#define NB_STRIPS 7
 // Enable controller TM1809 on PIN 6
 StripController<6, NUM_LEDS> LED;
 struct CRGB * pixels;
 
 LEDStrip strip[NB_STRIPS];
-RunningEffect effect[NB_STRIPS];
+FireEffect effect[NB_STRIPS];
 
 
 /**
@@ -26,33 +28,41 @@ void setup() {
 
     strip[0].setPixels(pixels, 90);
     effect[0].setStrip(&strip[0]);
-    effect[0].color  = Color(127, 127, 127);
+/*    effect[0].color  = Color(127, 127, 127);
     effect[0].length = 10;
     effect[0].rewind = true;
-    
+    */
     strip[1].setPixels(pixels + 90, 90);
     strip[1].setReverse(true);
     effect[1].setStrip(&strip[1]);
-    effect[1].color  = Color(255, 0, 0);
+/*    effect[1].color  = Color(255, 0, 0);
     effect[1].rewind = true;
     effect[1].length = 0;
-
+*/
     strip[2].setPixels(pixels + 180, 90);
     effect[2].setStrip(&strip[2]);
-    effect[2].color  = Color(0, 255, 0);
+/*    effect[2].color  = Color(0, 255, 0);
     effect[2].length = 1;
-    
+*/    
     strip[3].setPixels(pixels + 270, 90);
     strip[3].setReverse(true);
     effect[3].setStrip(&strip[3]);
-    effect[3].color  = Color(0, 0, 255);
+/*    effect[3].color  = Color(0, 0, 255);
     effect[3].length = 1;
-
+*/
     strip[4].setPixels(pixels + 360, 90);
     effect[4].setStrip(&strip[4]);
-    effect[4].color  = Color(255, 255, 0);
+/*    effect[4].color  = Color(255, 255, 0);
     effect[4].length = 10;
+  */  
+
+    strip[5].setPixels(pixels + 450, 90);
+    strip[5].setReverse(true);
+    effect[5].setStrip(&strip[5]);
     
+    strip[6].setPixels(pixels + 540, 90);
+    effect[6].setStrip(&strip[6]);
+
     LED.show();
     
     randomSeed(analogRead(0));
